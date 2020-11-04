6d5c39ff3847d48e219677a31d780f7b539396e1
Luke Eastman <LukeAEastman@gmail.com>
2020-11-04 12:43:56 -0800

    use full text search for searching db

--- challenge_1/client/components/HistoricalEventsFinder.jsx
+++ challenge_1/client/components/HistoricalEventsFinder.jsx
@@ -7,14 +7,14 @@ import Search from './Search.jsx';
 const HistoricalEventsFinder = () => {
   const [results, setResults] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
-
+  const [searchPage, setSearchPage] = useState(1);
   useEffect(() => {
     axios({
       method: 'get',
-      url: `http://localhost:3000/events?description_like=${searchTerm}`
+      url: `http://localhost:3000/events?_page=${searchPage}&q=${searchTerm}`
     })
     .then(result => {
-      console.log(result)
+      console.log(result.headers.link)
       setResults(result.data)
     })
   }, [searchTerm])
