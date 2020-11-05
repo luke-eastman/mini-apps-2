d5d2c9beffab31b4d327731ce20bf656f12a5c7f
Luke Eastman <LukeAEastman@gmail.com>
2020-11-05 10:40:45 -0800

    call api from view to get price data

--- challenge_2/client/components/Crypto.jsx
+++ challenge_2/client/components/Crypto.jsx
@@ -1,6 +1,23 @@
 import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 const Crypto = () => {
+  const [prices, setPrices] = useState([]);
+
+  useEffect(() => {
+    axios({
+      method: 'get',
+      url: '/api/closing'
+    })
+    .then(res => {
+      setPrices(res.data);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  }, []);
+
+
   return (
     <h1>More Crypto</h1>
   );
--- challenge_2/server/server.js
+++ challenge_2/server/server.js
@@ -5,7 +5,7 @@ const port = 3000;
 
 const dataController = require('./controllers/coinDesk.js')
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.get('/api/closing', (req, res) => {
   dataController.getClosingPrices()
