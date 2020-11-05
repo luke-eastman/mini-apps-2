e0f981515bbfcf33acce44b785cbac079fd1dc83
Luke Eastman <LukeAEastman@gmail.com>
2020-11-05 10:08:20 -0800

    set up react

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,2 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Crypto from './components/Crypto.jsx'
 
-
+ReactDOM.render(<Crypto />, document.getElementById('crypto'));
---
+++ challenge_2/client/components/Crypto.jsx
@@ -0,0 +1,9 @@
+import React, {useEffect, useState} from 'react';
+
+const Crypto = () => {
+  return (
+    <h1>More Crypto</h1>
+  );
+}
+
+export default Crypto;
